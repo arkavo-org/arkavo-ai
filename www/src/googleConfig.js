4b57ca2f85f3ed43b9251cb31563abc39f728f46
Julian Loiacono <jcloiacon@gmail.com>
2024-11-12 03:40:16 -0500

    Update googleConfig.js

--- www/src/googleConfig.js
+++ www/src/googleConfig.js
@@ -3,7 +3,7 @@
 export const googleConfig = {
     clientId: '628937582864-rcccrc973thjt8kiln9p0pti6usqksm4.apps.googleusercontent.com',
     clientSecret: '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',
-    redirectUri: 'http://arkavo.ai:5173/callback',
+    redirectUri: 'https://arkavo.ai/callback',
     //redirectUri: 'https://arkavo.ai',
     scopes: ['openid', 'profile', 'email'],
     authEndpoint: 'https://accounts.google.com/o/oauth2/v2/auth',
