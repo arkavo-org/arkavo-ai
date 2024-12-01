a7997047f8abc7af59d2badd36a5d4fce56627b2
Julian Loiacono <jcloiacon@gmail.com>
2024-12-01 10:20:22 -0500

    from server

---
+++ www/runDocker.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Get the last two levels of the directory structure
+PARENT_DIR=$(basename "$(dirname "$PWD")")
+CURRENT_DIR=$(basename "$PWD")
+export CONTAINER_NAME="${PARENT_DIR}-${CURRENT_DIR}"
+echo "CONTAINER NAME $CONTAINER_NAME"
+
+# Set FRONTEND_PORT based on the presence of 'dev' in COMPOSE_PROJECT_NAME
+if [[ $PARENT_DIR == "arkavo-ai-dev"	]]; then
+  export FRONTEND_PORT=3001
+else
+  export FRONTEND_PORT=3000
+fi
+echo "Frontend port set to $FRONTEND_PORT"
+
+# Start the Docker Compose stack
+docker network inspect arkavo >/dev/null 2>&1 || docker network create arkavo
+
+docker run -d \
+  --name "frontend_${CONTAINER_NAME}" \
+  --network arkavo \
+  --rm \
+  -v "$(pwd):/usr/src/app" \
+  -w /usr/src/app \
+  -p "${FRONTEND_PORT}:5173" \
+  -e NODE_ENV=development \
+  -e VITE_KEYCLOAK_SERVER_URL="${VITE_KEYCLOAK_SERVER_URL}" \
+  -e VITE_KEYCLOAK_REALM="${VITE_KEYCLOAK_REALM}" \
+  -e VITE_KEYCLOAK_CLIENT_ID="${VITE_KEYCLOAK_CLIENT_ID}" \
+  -e VITE_ORG_BACKEND_URL="${VITE_ORG_BACKEND_URL}" \
+  -e VITE_KAS_ENDPOINT="${VITE_KAS_ENDPOINT}" \
+  node:22 \
+  sh -c "npm install && npm run dev"
+
+
