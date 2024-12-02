3b9181c04f9385dcd7eb7c09b28abe2b1f2e100c
Julian Loiacono <jcloiacon@gmail.com>
2024-12-02 00:39:34 -0500

    from server

--- www/vite.config.ts
+++ www/vite.config.ts
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/dev/',
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     proxy: {
       '/api': {
         target: 'https://ollama.juliancoy.us',
